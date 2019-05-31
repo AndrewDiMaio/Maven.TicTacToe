303601b0fc3717c8bd83d2db8dcfd8d588251b8c
andrewdimaio <andrewdimaio777@gmail.com>
2019-05-31 12:49:23 -0400

    All tests pass

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -6,22 +6,22 @@ package rocks.zipcodewilmington.tictactoe;
 public class Board {
     String outcome = "";
     public Board(Character[][] matrix) {
-        if (matrix[0][1].equals(matrix[0][0]) && (matrix[0][1].equals(matrix[0][2]))){
-           outcome +=  matrix[0][1];
-        }else if (matrix[1][1].equals(matrix[1][0]) && (matrix[1][1].equals(matrix[1][2]))){
-           outcome +=  matrix[1][1];
-        }else if (matrix[2][1].equals(matrix[2][0]) && (matrix[2][1].equals(matrix[2][2]))){
-            outcome +=  matrix[2][1];
-        }else if (matrix[0][2].equals(matrix[1][1]) && (matrix[0][2].equals(matrix[2][0]))){
-            outcome +=  matrix[0][2];
-        }else if (matrix[0][1].equals(matrix[1][1]) && (matrix[0][1].equals(matrix[2][1]))){
-            outcome +=  matrix[0][1];
-        }else{if (matrix[1][2].equals(matrix[0][2]) && (matrix[1][2].equals(matrix[2][2]))){
-            outcome +=  matrix[1][2];
-        }else if (matrix[0][0].equals(matrix[1][1]) && (matrix[0][0].equals(matrix[2][2]))){
-            outcome +=  matrix[0][0];
-        }else if (matrix[0][0].equals(matrix[1][0]) && (matrix[0][0].equals(matrix[2][0]))){
-             outcome +=  matrix[0][0];
+        if (matrix[0][1].equals(matrix[0][0]) && (matrix[0][1].equals(matrix[0][2])) && !matrix[0][1].equals(' ')){
+           outcome +=  matrix[0][1];    //top row
+        }else if (matrix[1][0].equals(matrix[1][1]) && (matrix[1][1].equals(matrix[1][2]))&& !matrix[1][1].equals(' ')){
+           outcome +=  matrix[1][0];    //middle row
+        }else if (matrix[2][1].equals(matrix[2][0]) && (matrix[2][1].equals(matrix[2][2]))&& !matrix[2][2].equals(' ')){
+            outcome +=  matrix[2][1];   //bottom row
+        }else if (matrix[0][2].equals(matrix[1][1]) && (matrix[0][2].equals(matrix[2][0]))&& !matrix[0][2].equals(' ')){
+            outcome +=  matrix[0][2];   //top right to bottom left
+        }else if (matrix[0][1].equals(matrix[1][1]) && (matrix[0][1].equals(matrix[2][1]))&& !matrix[0][1].equals(' ')){
+            outcome +=  matrix[0][1];   //middle column
+        }else{if (matrix[1][2].equals(matrix[0][2]) && (matrix[1][2].equals(matrix[2][2]))&& !matrix[1][2].equals(' ')){
+            outcome +=  matrix[1][2];   //third column
+        }else if (matrix[0][0].equals(matrix[1][1]) && (matrix[0][0].equals(matrix[2][2]))&& !matrix[0][0].equals(' ')){
+            outcome +=  matrix[0][0];   //top left to bottom right
+        }else if (matrix[0][0].equals(matrix[1][0]) && (matrix[0][0].equals(matrix[2][0]))&& !matrix[0][0].equals(' ')){
+             outcome +=  matrix[0][0];  //first column
 
         }else{outcome += "";
         }
